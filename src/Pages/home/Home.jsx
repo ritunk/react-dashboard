e90408c39a875fd4962477822a1489e3d8fc25e7
ritik185 <145895219+ritunk@users.noreply.github.com>
2024-06-20 00:08:21 +0530

    widget update

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -37,6 +37,45 @@
 
 // export default Home;
 
+// import React from "react";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import "./home.scss";
+// import Navbar from "../../components/navbar/Navbar";
+// import Widget from "../../components/widget/Widget";
+// import Featured from "../../components/featured/Featured";
+// import Chart from "../../components/chart/Chart";
+// import List from "../../table/Table";
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <Sidebar />
+
+//       <div className="homeContainer">
+//         <Navbar />
+//         <div className="widgets">
+//           <Widget type="user" className="widg" />
+//           <Widget type="order" className="widg" />
+//           <Widget type="earning" className="widg" />
+//           <Widget type="balance" className="widg" />
+//         </div>
+
+//         <div className="charts">
+//           <Featured />
+//           <Chart title="Last 6 months revenue" aspect={2 / 1} />
+//         </div>
+
+//         <div className="listContainer">
+//           <div className="listTitle">Latest Transactions</div>
+//           <List />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
@@ -54,10 +93,10 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user" className="widg" />
-          <Widget type="order" className="widg" />
-          <Widget type="earning" className="widg" />
-          <Widget type="balance" className="widg" />
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" /> {/* Corrected the type here */}
+          <Widget type="balance" />
         </div>
 
         <div className="charts">
