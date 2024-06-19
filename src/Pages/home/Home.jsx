3d4522eca86d7d35da6976632d55a2c511485a08
ritik185 <145895219+ritunk@users.noreply.github.com>
2024-06-19 23:52:36 +0530

    Widget update

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,3 +1,42 @@
+// import React from "react";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import "./home.scss";
+// import Navbar from "../../components/navbar/Navbar";
+// import Widget from "../../components/widget/Widget";
+// import Featured from "../../components/featured/Featured";
+// import Chart from "../../components/chart/Chart";
+// import List from "../../table/Table";
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <Sidebar />
+
+//       <div className="homeContainer">
+//         <Navbar />
+//         <div className="widgets">
+//           <Widget type="user" />
+//           <Widget type="order" />
+//           <Widget type="erarning" />
+//           <Widget type="balance" />
+//         </div>
+
+//         <div className="charts">
+//           <Featured />
+//           <Chart title="Last 6 month revenue" aspect={2 / 1} />
+//         </div>
+
+//         <div className="listContainer">
+//           <div className="listTitle">Latest Transactions</div>
+//           <List />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
@@ -15,15 +54,15 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user" />
-          <Widget type="order" />
-          <Widget type="erarning" />
-          <Widget type="balance" />
+          <Widget type="user" className="widg" />
+          <Widget type="order" className="widg" />
+          <Widget type="earning" className="widg" />
+          <Widget type="balance" className="widg" />
         </div>
 
         <div className="charts">
           <Featured />
-          <Chart title="Last 6 month revenue" aspect={2 / 1} />
+          <Chart title="Last 6 months revenue" aspect={2 / 1} />
         </div>
 
         <div className="listContainer">
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,3 +1,105 @@
+// import React from "react";
+// import "./widget.scss";
+// import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+// import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+// import {
+//   AccountBalanceOutlined,
+//   MonetizationOnOutlined,
+//   ShoppingCartCheckoutOutlined,
+// } from "@mui/icons-material";
+
+// const Widget = ({ type }) => {
+//   let data;
+
+//   //tomparay
+//   const amount = 100;
+//   const diff = 20;
+
+//   switch (type) {
+//     case "user":
+//       data = {
+//         title: "USERS",
+//         isMoney: false,
+//         link: "see all users",
+//         icon: (
+//           <PersonOutlineIcon
+//             className="icon"
+//             style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+//           />
+//         ),
+//       };
+//       break;
+
+//     case "erarning":
+//       data = {
+//         title: "ERARNINGS",
+//         isMoney: true,
+//         link: "View net earnings",
+//         icon: (
+//           <MonetizationOnOutlined
+//             className="icon"
+//             style={{
+//               color: "goldenrod  ",
+//               backgroundColor: "rgba(218,165,32,0.2)",
+//             }}
+//           />
+//         ),
+//       };
+//       break;
+
+//     case "order":
+//       data = {
+//         title: "ORDERS",
+//         isMoney: false,
+//         link: "View all orders",
+//         icon: (
+//           <ShoppingCartCheckoutOutlined
+//             className="icon"
+//             style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+//           />
+//         ),
+//       };
+//       break;
+
+//     case "balance":
+//       data = {
+//         title: "BALANCE",
+//         isMoney: true,
+//         link: "see details",
+//         icon: (
+//           <AccountBalanceOutlined
+//             className="icon"
+//             style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+//           />
+//         ),
+//       };
+//       break;
+
+//     default:
+//       break;
+//   }
+//   return (
+//     <div className="widget">
+//       <div className="left">
+//         <span className="title">{data.title}</span>
+//         <span className="counter">
+//           {data.isMoney && "$"} {amount}
+//         </span>
+//         <span className="link">{data.link}</span>
+//       </div>
+//       <div className="right">
+//         <div className="percentage positive">
+//           <KeyboardArrowUpIcon />
+//           {diff}%
+//         </div>
+//         {data.icon}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Widget;
+
 import React from "react";
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
@@ -11,7 +113,7 @@ import {
 const Widget = ({ type }) => {
   let data;
 
-  //tomparay
+  // Temporary values
   const amount = 100;
   const diff = 20;
 
@@ -20,7 +122,7 @@ const Widget = ({ type }) => {
       data = {
         title: "USERS",
         isMoney: false,
-        link: "see all users",
+        link: "See all users",
         icon: (
           <PersonOutlineIcon
             className="icon"
@@ -30,16 +132,16 @@ const Widget = ({ type }) => {
       };
       break;
 
-    case "erarning":
+    case "earning": // Corrected the typo here
       data = {
-        title: "ERARNINGS",
+        title: "EARNINGS",
         isMoney: true,
         link: "View net earnings",
         icon: (
           <MonetizationOnOutlined
             className="icon"
             style={{
-              color: "goldenrod  ",
+              color: "goldenrod",
               backgroundColor: "rgba(218,165,32,0.2)",
             }}
           />
@@ -65,7 +167,7 @@ const Widget = ({ type }) => {
       data = {
         title: "BALANCE",
         isMoney: true,
-        link: "see details",
+        link: "See details",
         icon: (
           <AccountBalanceOutlined
             className="icon"
@@ -76,8 +178,15 @@ const Widget = ({ type }) => {
       break;
 
     default:
+      data = {
+        title: "UNKNOWN",
+        isMoney: false,
+        link: "See details",
+        icon: <PersonOutlineIcon className="icon" />,
+      };
       break;
   }
+
   return (
     <div className="widget">
       <div className="left">
